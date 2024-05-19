070f39b327802641ac142ee18938db77e5220985
khoavu2502 <khoavu2502@gmail.com>
2024-05-19 11:50:05 +0700

    khoavu2502/Implement searching products by keyword api

--- backend/src/main/java/com/dev/backend/rest/CategoryRestController.java
+++ backend/src/main/java/com/dev/backend/rest/CategoryRestController.java
@@ -13,6 +13,8 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
+
 public class CategoryRestController {
 
     private final CategoryService categoryService;
